8d58ec338adeb2d794c39e5329502e02f7eea9fb
Fran Avila <fran@Frans-MacBook-Pro.local>
2021-05-02 22:00:27 +0200

    Cleaning formatting. Extract static methods.

--- src/pairDeterminer.py
+++ src/pairDeterminer.py
@@ -3,34 +3,36 @@ from handValue import HandValue
 from collections import Counter
 
 
-
-
 class PairDeterminer:
 
     def __init__(self):
         self._cards_in_a_pair = 2
 
-    def _sort_hand(self, hand):
+    @staticmethod
+    def _sort_hand(hand):
         hand.sort(key=lambda x: x.face_value.value, reverse=True)
 
-    def _count_items(self, x, counter):
-        return (x, counter[x])
+    @staticmethod
+    def _count_items(x, counter):
+        return x, counter[x]
 
-    def _orderedSet(self, seq):
+    @staticmethod
+    def _ordered_set(seq):
         seen = set()
         seen_add = seen.add
         return [x for x in seq if not (x in seen or seen_add(x))]
 
+    @staticmethod
+    def _add_cards_to_rest(rest_cards, card_value, amount_to_add):
+        for y in range(0, amount_to_add):
+            rest_cards.append(card_value[0])
+
     def _group_hand_by_value_repetition(self, hand):
         self._sort_hand(hand)
         face_values = list(map(lambda x: x.face_value, hand))
-        unique_face_values = self._orderedSet(face_values)
+        unique_face_values = self._ordered_set(face_values)
         counter = Counter(face_values)
         return list(map(lambda x: self._count_items(x, counter), unique_face_values))
-
-    def _add_cards_to_rest(self, rest_cards, card_value, amount_to_add):
-        for y in range(0, amount_to_add):
-            rest_cards.append(card_value[0])
 
     def determine(self, hand):
         rest_cards = []
@@ -45,11 +47,9 @@ class PairDeterminer:
                 if card_value[1] >= self._cards_in_a_pair:
                     pair_found = True
                     value = card_value[0]
-                    amount = card_value[1]-self._cards_in_a_pair
+                    amount = card_value[1] - self._cards_in_a_pair
             self._add_cards_to_rest(rest_cards, card_value, amount)
 
         if pair_found:
             return HandValue(Ranking.PAIR, value, rest_cards)
         return HandValue(Ranking.HIGH_CARD, grouped_hand[0][0], rest_cards)
-
-
